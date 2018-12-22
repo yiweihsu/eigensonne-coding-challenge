6ecbd15cdcb6f95bfc3e02008f683b161edfd1ec
Yi-Wei Hsu <yiweihsutw@gmail.com>
2018-12-22 14:52:38 +0100

    refactor API fetcher service and unit testing

--- src/Services/ApiFetcherService.php
+++ src/Services/ApiFetcherService.php
@@ -7,49 +7,9 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class ApiFetcherService
 {
-	// https://hacker-news.firebaseio.com/v0/topstories.json
-	// topstories
-	// newstories
-	// beststories
-	// askstories
-	// showstories
-	// jobstories
-	
-	private $typeArray = array(
-		'topstories',
-		'newstories',
-		'beststories',
-		'askstories',
-		'showstories',
-		'jobstories'
-	);
-
-	private $baseUrl = 'https://hacker-news.firebaseio.com/v0/';
-
-  public function getItems($apiUrl)
-  {
+	public function getData($url) {
 		$client = new Client();
-		$res = $client->request('GET', $apiUrl);
+		$res = $client->request('GET', $url);
 		return $res;
-  }
-
-  public function getItemById($apiUrl, $id)
-  {
-		$client = new Client();
-		$res = $client->request('GET', $apiUrl . $id . '.json');
-		return $res;
-  }
-
-  public function getStories($dataType) 
-  {
-		$typeArray = $this->typeArray;
-		if (!in_array($dataType, $typeArray)) {
-			return;
-		}
-
-		$baseUrl = $this->baseUrl;
-		$client = new Client();
-		$res = $client->request('GET', $baseUrl . $dataType . '.json');
-		return $res;
-  }
+	}
 }
