a3153c4d8a95cd33cc9ac46c18133b21cacabe71
Yi-Wei Hsu <yiweihsutw@gmail.com>
2018-12-22 01:19:52 +0100

    setup testing environment

---
+++ tests/Services/ApiFetcherTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace \tests\Services;
+
+use Services\ApiFetcher;
+use PHPUnit\Framework\TestCase;
+
+class ApiFetcherTest extends TestCase {
+
+    public function testApiFetcher()
+    {
+    }
+}
