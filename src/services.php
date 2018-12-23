bb93e5e50a8c5faa3ca70b3c47bdb6b506231846
Yi-Wei Hsu <yiweihsutw@gmail.com>
2018-12-23 21:48:06 +0100

    refactor service because of removing data processor

--- src/services.php
+++ src/services.php
@@ -1,23 +1,18 @@
 <?php
 
-use Services\ApiFetcherService;
 use Services\UrlMapperService;
-use Services\DataProcessorService;
+use Services\ApiFetcherService;
 use Services\ServiceCallerService;
 use GuzzleHttp\Client;
-
-$app['api.fetcher.service'] = function ($app) {
-  return new ApiFetcherService();
-};
 
 $app['url.mapper.service'] = function ($app) {
   return new UrlMapperService();
 };
 
-$app['data.processor.service'] = function ($app) {
-  return new DataProcessorService();
+$app['api.fetcher.service'] = function ($app) {
+  return new ApiFetcherService();
 };
 
 $app['service.caller.service'] = function ($app) {
-  return new ServiceCallerService();
+  return new ServiceCallerService($app['url.mapper.service'], $app['api.fetcher.service']);
 };
