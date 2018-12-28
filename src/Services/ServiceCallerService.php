cc40ef60ea2b717b29baf24f57179cb933523323
Yi-Wei Hsu <yiweihsutw@gmail.com>
2018-12-28 13:52:46 +0100

    complete pagination

--- src/Services/ServiceCallerService.php
+++ src/Services/ServiceCallerService.php
@@ -22,15 +22,25 @@ class ServiceCallerService
     $url = $this->urlMapperService->getUrl($dataType);
     $data = $this->apiFetcherService->getData($url);
     $jsonData = json_decode($data->getBody(), TRUE);
-    $resultForTemplate = $this->itemLoopFetcher($jsonData);
+
+    // limit the amount of data retrival depending on the page
+    $pageParams = basename($_SERVER['REQUEST_URI']);
+    $pageNum = (int) substr($pageParams, -1);
+    // var_dump($pageNum);
+    $beginIndexOfArray = 0;
+    if ($pageNum > 1) {
+      $beginIndexOfArray = ($pageNum - 1) * 30;
+    }
+
+    $resultForTemplate = $this->itemLoopFetcher($jsonData, $beginIndexOfArray);
     return $resultForTemplate;
   }
 
   // pass the page as parameter
-  private function itemLoopFetcher($data) { 
+  private function itemLoopFetcher($data, $beginIndexOfArray) { 
     $dataArr = [];
-    $i = 0;
-    while($i < 10) { // TODO make it faster
+    $i = $beginIndexOfArray;
+    while($i < $beginIndexOfArray + 30) { // TODO make it faster
       $itemUrl = $this->urlMapperService->getItemUrl($data[$i]);
       $result = $this->apiFetcherService->getData($itemUrl);
       $jsonResult = json_decode($result->getBody(), TRUE);
